63853a43b35811cc3fcc3c2a58b89d8a35621c22
Debbie Urquhart <debiurquhart@live.co.uk>
2020-05-16 14:39:21 +0100

    adds table with data to users

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -22,7 +22,7 @@ export class AuthGuard implements CanActivate, CanLoad {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return false;
+    return true;
   }
   canLoad(
     route: Route,
