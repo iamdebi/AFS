255788ecdaa5d2933c4ea419c28371741f38879e
Debbie Urquhart <debiurquhart@live.co.uk>
2020-05-16 19:55:03 +0100

    adds list and input to client

--- src/app/home/clients/clients.component.ts
+++ src/app/home/clients/clients.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as queries from "../../../graphql/queries";
+import * as mutations from "../../../graphql/mutations";
+import * as subscriptions from "../../../graphql/subscriptions";
+import { API, graphqlOperation } from "aws-amplify";
+import { APIService } from "../../API.service";
 
 @Component({
-  selector: 'app-clients',
-  templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.scss']
+  selector: "app-clients",
+  templateUrl: "./clients.component.html",
+  styleUrls: ["./clients.component.scss"]
 })
 export class ClientsComponent implements OnInit {
+  allTodos: Array<any>;
 
-  constructor() { }
+  constructor(private apiService: APIService) {}
 
-  ngOnInit() {
+  createTodo(name, description) {
+    this.apiService.CreateTodo({
+      name: name,
+      description: description
+    });
   }
 
+  async ngOnInit() {
+    this.apiService.ListTodos().then(evt => {
+      this.allTodos = evt.items;
+    });
+
+    this.apiService.OnCreateTodoListener.subscribe(evt => {
+      const data = (evt as any).value.data.onCreateTodo;
+      this.allTodos = [...this.allTodos, data];
+    });
+  }
 }
